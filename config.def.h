af18429014bfac1fe77dfc74526f2488a44797a6
phamhiep2506 <phamhiep25062k@gmail.com>
2022-12-29 02:58:46 +0700

    update color

--- config.def.h
+++ config.def.h
@@ -8,11 +8,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#16161e";
+static const char col_gray2[]       = "#565f89";
+static const char col_gray3[]       = "#c0caf5";
+static const char col_gray4[]       = "#1a1b26";
+static const char col_cyan[]        = "#9d7cd8";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
