656b72bb5adde60d479307c17061928d17353199
phamhiep2506 <phamhiep25062k@gmail.com>
2022-12-31 05:01:32 +0700

    add shortcut rofi

--- config.h
+++ config.h
@@ -65,7 +65,8 @@ static const char *termcmd[]  = { "kitty", NULL };
 #include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          SHCMD("~/.config/rofi/launcher") },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
