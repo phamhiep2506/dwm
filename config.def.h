33c64cd14bc6be508730f4ee9f9d71576f0abe20
phamhiep2506 <phamhiep25062k@gmail.com>
2023-01-07 12:16:48 +0700

    change tag name

--- config.def.h
+++ config.def.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -35,7 +35,6 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-  { "mpv",      NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
